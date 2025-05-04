5d80dad16451e027abc5af1aea693b992a6d74b7
Chaima Maami <cmaami@c3r7p5.1337.ma>
2025-05-04 18:10:25 +0100

    push

---
+++ ccc/main.cpp
@@ -0,0 +1,14 @@
+#include "client.hpp"
+
+int main()
+{
+    try
+    {
+        client client("GET / HTTP/2.6\r\nhhh: ds\r\n", 0);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+}
