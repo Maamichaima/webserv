3811fc74169b485ad2769c46845143c4452ecdf9
Chaima Maami <cmaami@e1r5p12.1337.ma>
2025-05-08 16:58:31 +0100

    start line && headrs done

--- ccc/parcere.hpp
+++ ccc/parcere.hpp
@@ -1,16 +1,22 @@
 #pragma once
 
 #include <iostream>
-
+struct data_request;
+class client;
 class parcere
 {
     public:
         
     public:
-        parcere(std::string str, int &flag);
+        void parcHttpCall(client &client);
         parcere();
         parcere(const parcere &obj);
         parcere &operator=(const parcere &obj);
         ~parcere();
+        int parce(client &client);
+        void setDateToStruct(data_request &data_rq, std::string &buffer, int flag);
 };
-int parce(std::string http_rq, int &flag);
+int parce_header(std::string str);
+int pars_startligne(std::string str);
+std::string get_line(std::string str);
+#include "client.hpp"
