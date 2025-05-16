77c084b3000af99a44e0e7b07ff7c4040d0f0521
rehab <rihabraida@gmail.com>
2025-05-16 19:14:23 +0100

    check valid keys in locations + add a function to get the values of a specific key in location

--- _includes/ServerManager.hpp
+++ _includes/ServerManager.hpp
@@ -10,36 +10,23 @@
 class ServerManager{
 
     private:
-    std::vector<Server> servers;
-    std::map<int,client> clients;
-    static size_t numServer ;
-    int epollFd;
-    struct epoll_event events[MAX_EVENTS];
+        std::vector<Server> servers;
+        std::map<int,client> clients;
+        static size_t numServer ;
+        int epollFd;
+        struct epoll_event events[MAX_EVENTS];
     public:
         ServerManager();
-        std::vector<Server>  get_servers();
-        std::map<int,client>  get_clients();
-        size_t  getNumServer();
-        void addServer(Server& server);
-        bool initializeAll() ;
-
-        std::vector<int> getAllSocketFds() ;
-        Server* findServerByFd(int fd) ;
-        void printAllServerInfo() ;
-
-        bool Add_new_event(int fd_socket);
-        // bool find_client_server(int fd)
-        // {
-        //     for(size_t i = 0;i < servers.size() ;i++)
-        //     {
-        //         for(size_t i = 0;i < servers.size() ;i++)
-        //         {
-
-        //         }
-        //     }
-        // }
-
-       void handle_cnx();
+        std::vector<Server>     get_servers();
+        std::map<int,client>    get_clients();
+        size_t                  getNumServer();
+        void                    addServer(Server& server);
+        bool                    initializeAll() ;
+        std::vector<int>        getAllSocketFds() ;
+        Server*                 findServerByFd(int fd) ;
+        void                    printAllServerInfo() ;
+        bool                    Add_new_event(int fd_socket);
+        void                    handle_cnx();
 
 
 
