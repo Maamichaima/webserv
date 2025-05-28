23936c2f97e646f35cc26d7c656c02bcc668c942
Mohamed Amine Manaoui <mohamedamine.manaoui@gmail.com>
2025-05-28 21:07:52 +0100

    handle cgi but bzaaaaf dial les errors

---
+++ www/xphp/file.php
@@ -0,0 +1,14 @@
+<?php
+// This is a simple PHP script
+
+// Define a variable
+$name = "World";
+
+// Print a message
+echo "Hello, $name!";
+
+// Basic if statement
+if ($name == "World") {
+    echo " Welcome to PHP programming!";
+}
+?>
