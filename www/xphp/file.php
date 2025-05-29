7d431b580b5ccfad4706f263da74da42862833d4
Mohamed Amine Manaoui <mohamedamine.manaoui@gmail.com>
2025-05-29 13:25:53 +0100

    modifed interpreter in config file

--- www/xphp/file.php
+++ www/xphp/file.php
@@ -1,13 +1,10 @@
 <?php
 // This is a simple PHP script
 
-// Define a variable
 $name = "World";
 
-// Print a message
 echo "Hello, $name!";
 
-// Basic if statement
 if ($name == "World") {
     echo " Welcome to PHP programming!";
 }
