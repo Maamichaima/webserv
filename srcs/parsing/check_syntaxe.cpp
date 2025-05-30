23952e12a35181926554ac17eefb81271b9c8689
rehab <rihabraida@gmail.com>
2025-05-30 22:50:35 +0100

    test

--- srcs/parsing/check_syntaxe.cpp
+++ srcs/parsing/check_syntaxe.cpp
@@ -2,7 +2,7 @@
 #include "../../_includes/Server.hpp"
 
 
-bool   validate_port(const std::string& port){
+bool   valide_port(const std::string& port){
 
     for(int i  ; i < port.size() ;i++){
         char c = port[i];
@@ -70,53 +70,25 @@ bool check_listen( std::vector<std::string> values,Server &server)
     while(it != values.end())
     {
         std::string value = *it;
-        size_t colon_pos = value.find(':');
+        if (!valide_port(value))
+            return false;
+        server.set_Port(value);
+        ++it;
+    }
+    return true;
+}
 
-        if(colon_pos != std::string::npos)
-        {
-            std::string host = value.substr(0, colon_pos);
-            std::string port = value.substr(colon_pos + 1);
-            if (!host.empty()) // listen :PORT
-            {   
-                if (!valide_ip_address(host))
+bool check_host(std::vector<std::string> values,Server &server)
+{
+    if(values.empty())
+        return false;
+    std::vector<std::string>::iterator it = values.begin();
+    while(it != values.end())
+    {
+        std::string value = *it;
+        if (!valide_ip_address(value))
                     return false;
-                server.set_IpAddress(host);
-            }
-            else
-                return false;
-            if(!port.empty())
-            {
-                if (!validate_port(port))
-                    return false;
-                server.set_Port(port);
-            }
-            else
-                return false;
-        }
-        else
-        {
-            bool is_port = true; 
-            for (int i  ; i < value.size() ;i++)
-            {
-                char c = value[i];
-                if (!isdigit(c))
-                {
-                    is_port = false;
-                    break;
-                }
-            }
-            if (is_port)
-            {
-                if (!validate_port(value))
-                    return false;
-                server.set_Port(value);
-            }
-            else
-            {    if (!valide_ip_address(value))
-                    return false;
-                server.set_IpAddress(value);
-            }
-        }
+         server.set_IpAddress(value);
         ++it;
     }
     return true;
@@ -127,7 +99,7 @@ bool check_server_names(std::vector<std::string> values)
     if (values.empty())
         return false;
         
-    for (int i  ; i < values.size() ;i++)
+    for (int i = 0 ; i < values.size() ;i++)
     {
         std::string name = values[i];
         if (name.empty())
@@ -204,17 +176,14 @@ bool check_client_max_body_size(std::vector<std::string> values){
 bool    param_Syntaxe(std::string key, std::vector<std::string> values,Server &server){ // it = params.begin()
     
 
-    if(key =="listen"){
+    if(key =="listen")
         return (check_listen(values,server));
-    }
-    else if(key == "server_name" || key == "server_names" ){ //Setup the server_names or not.
+    else if(key == "host")
+        return(check_host(values,server));
+    else if(key == "server_name" || key == "server_names" )//Setup the server_names or not.
         return(check_server_names(values));
-    }
     else if( key == "error_page")
-    {
         return(check_error_page(values));
-
-    }
     else if(key =="client_max_body_size" )
         return(check_client_max_body_size(values));
     else
