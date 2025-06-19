26e5a5312c8abbf34cf4558144f145a05f29e1cb
Mohamed Amine Manaoui <mohamedamine.manaoui@gmail.com>
2025-06-19 12:59:34 +0100

    cgiwerww

---
+++ www/xphp/index.php
@@ -0,0 +1,14 @@
+<?php
+// This is a simple PHP script
+
+// Define a variable
+$name = "World";
+
+// Define a function
+function greet($name) {
+    return "Hello, " . $name . "!";
+}
+
+// Call the function and display the result
+echo greet($name);
+?>
