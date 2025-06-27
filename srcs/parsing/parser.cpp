e7a8023f14e026dece7ebf582534f7dfcd79fbd7
Chaima Maami <cmaami@c3r1p4.1337.ma>
2025-06-27 12:13:09 +0100

    test in lunix

--- srcs/parsing/parser.cpp
+++ srcs/parsing/parser.cpp
@@ -3,7 +3,7 @@
 #include "../methods/PostMethod.hpp"
 #include "../methods/GetMethod.hpp"
 #include "../methods/DeleteMethod.hpp"
-// #include "parser.hpp"
+
 parser::parser()
 {
 }
@@ -22,82 +22,52 @@ parser::~parser()
 {
 }
 
-std::string get_line_size(std::string str, int size)
-{
-	std::string sub;
-
-	if(size > str.size())
-		return str;
-	sub = str.substr(0, size);
-		return sub;
-}
-
-int isNumber(std::string str)
-{
-	int i = 0;
-	while(str[i])
-	{
-		if(!isalnum(str[i]))
-			return 0;
-		i++;
-	}
-	return 1;
-}
-
 int parser::parse(client &client)
 {
-	if(client.flag == 0)
+	if(client.flagProgress == 0)
 	{
 		std::string start_line = get_line(client.buffer);
 		if(start_line.find("\r\n") == std::string::npos)
-		{
 			return -2;
-		}
 		else if(parse_startligne(start_line))
 		{
-			this->setDateToStruct(client, start_line, client.flag);
-			client.flag = 1;
+			this->setDateToStruct(client, start_line);
+			client.flagProgress = 1;
 			client.buffer.erase(0, start_line.size());
 			if(client.data_rq.version != "HTTP/1.1")
-				throw (505);
+				throw (505); //HTTP Version Not Supported
 		}
 		else
-		{
-			// std::cout << "=====" << start_line << "===== start line problem \n";
-			throw (400);
-		}
+			throw (400); //Bad Request start line probl
 	}
-	if(client.flag == 1)
+	if(client.flagProgress == 1)
 	{
 		std::string header = get_line(client.buffer);
 		while (header != "\r\n" && header.find("\r\n") != std::string::npos)
 		{
 			if(parse_header(header))
 			{
-				this->setDateToStruct(client, header, client.flag);
+				this->setDateToStruct(client, header);
 				client.buffer.erase(0, header.size());
 			}
 			else
-			{
-				std::cout << header << " headers problem \n";
-				throw (400);
-			}
+				throw (400);//Bad Request start headers probl
 			header = get_line(client.buffer);
 		}
 		if(header == "\r\n")
 		{
 			client.check_http_body_rules();
-			client.flag = 2;
+			client.flagProgress = 2;
 			client.buffer.erase(0, header.size());
             client.data_rq.bodyNameFile = RandomString(5);
 			std::map<std::string, std::vector<std::string> >::iterator it = client.data_rq.myCloseLocation->infos.find("allowed_methods");
 			if(it != client.data_rq.myCloseLocation->infos.end() && std::find(it->second.begin(), it->second.end(), client.data_rq.method) == it->second.end())
-				throw (405);
+				throw (405); // Method Not Allowed
 			if(client.data_rq.method == "DELETE")
 				deleteMethode(client);
 		}
 	}
-	if(client.flag == 2)
+	if(client.flagProgress == 2)
 	{
 		if(client.data_rq.is_chunked == 1)
 		{
@@ -110,13 +80,13 @@ int parser::parse(client &client)
 					client.buffer.erase(0, body.size());
 				}
 				else
-					throw (400);
+					throw (400); //Bad Request
 			}
 			else if(client.data_rq.size_chunked > 0)
 			{
 				std::string body = get_line_size(client.buffer, client.data_rq.size_chunked);
 				client.data_rq.size_chunked -= body.size();
-				this->setDateToStruct(client, body, client.flag);
+				this->setDateToStruct(client, body);
 				client.buffer.erase(0, body.size());
 			}
 			else if(client.data_rq.size_chunked == 0)
@@ -127,52 +97,31 @@ int parser::parse(client &client)
 					client.data_rq.size_chunked = -1;
 					client.buffer.erase(0, body.size());
 					if (client.data_rq.flag_chunked == 0)
-						client.flag = 3;
+						client.flagProgress = 3;
 				}
 				else
-					throw (400);
+					throw (400); //Bad Request
 			}
 		}
 		else 
 		{
 			if(client.data_rq.size_body)
 			{
-				std::string body = get_line_size(client.buffer, client.data_rq.size_body);//hdgfbh
-				this->setDateToStruct(client, body, client.flag);
+				std::string body = get_line_size(client.buffer, client.data_rq.size_body);
+				this->setDateToStruct(client, body);
 				client.data_rq.size_body -= body.size();
 				client.buffer.erase(0, body.size());
 			}
 			if(client.data_rq.size_body == 0)
-				client.flag = 3;
+				client.flagProgress = 3;
 		}
 	}
 	return -1;
 }
 
-void toLower(std::string &str)
+void parser::setDateToStruct(client &client, std::string &buffer)//const ??
 {
-    int i = 0;
-
-    while(str[i])
-    {
-        str[i] = tolower(str[i]);
-        i++;
-    }
-}
-
-void checkKeyValueContent(std::string key, std::string value)
-{
-	key = trim(key);
-	value = trim(value);
-	if(key.length() == 0 || value.length() == 0)
-		throw(400);
-	if(!isMatch("[ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'*+\\-.^_`|~]+", key))
-		throw(400);
-}
-
-void parser::setDateToStruct(client &client, std::string &buffer, int flag)//const ??
-{
-    if(flag == 0)
+    if(client.flagProgress == 0)
     {
         std::string str = get_line(buffer);
         std::deque<std::string> startLine = split(str, " ");
@@ -189,7 +138,7 @@ void parser::setDateToStruct(client &client, std::string &buffer, int flag)//con
 			client.data_rq.path = startLine[1];
         client.data_rq.version = startLine[2].substr(0, startLine[2].size() - 2);
     }
-    if(flag == 1)
+    if(client.flagProgress == 1)
     {
 		std::string str = get_line(buffer);
 		size_t pos = str.find(":");
@@ -204,7 +153,7 @@ void parser::setDateToStruct(client &client, std::string &buffer, int flag)//con
 			client.myServer = *chooseServer(SocketToServers[client.server_fd],client.data_rq.headers["host"]);
 			client.data_rq.myCloseLocation = getClosestLocation(client.myServer, client.data_rq.path);
 			if(client.data_rq.myCloseLocation == NULL)
-				throw(404);
+				throw(404); //Not Found
 			std::map<std::string, std::vector<std::string> >::iterator it = client.data_rq.myCloseLocation->infos.find("redirect");
 			if(it != client.data_rq.myCloseLocation->infos.end())
 			{
@@ -214,7 +163,7 @@ void parser::setDateToStruct(client &client, std::string &buffer, int flag)//con
 			}
 		}
     }
-    if(flag == 2)
+    if(client.flagProgress == 2)
     {
 		if(client.data_rq.method == "POST")
             post(client, buffer);
